3248c2e9912e0066d4d3dae6e4d08e193c10e1d8
Nedeljko Milinkovic <nedeljko.milinkovic@bravostudio.com>
2020-01-25 18:31:15 +0100

    Remove env

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+echo "HELLOO!"
 echo "$VAULT_PASS" > ~/.vault_pass.txt
 mkdir ~/.ssh
 ansible-vault --vault-password-file ~/.vault_pass.txt view ansible/ssh_key.txt.secret > ~/.ssh/id_rsa
