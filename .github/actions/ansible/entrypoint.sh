7902e1ffeeb9102abd661602de8e45d3ad33940f
Nedeljko Milinkovic <nedeljko.milinkovic@bravostudio.com>
2020-01-30 18:58:59 +0100

    update actions

--- .github/actions/ansible/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "HELLOO!"
-echo "$VAULT_PASS" > ~/.vault_pass.txt
-mkdir ~/.ssh
-ansible-vault --vault-password-file ~/.vault_pass.txt view ansible/ssh_key.txt.secret > ~/.ssh/id_rsa
-chmod 0600 ~/.ssh/id_rsa
-echo "$pwd"
-ansible-playbook -e "build_sha=$GITHUB_SHA" --vault-password-file ~/.vault_pass.txt -i ansible/hosts ansible/deploy.yml
